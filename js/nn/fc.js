2dc3433c8718d2d2f6ec9e76c99f091831137813
Nils Gu <info@nilsgumpfer.com>
2024-10-11 15:07:53 +0200

    2024-10-11_15:07:53

--- js/nn/fc.js
+++ js/nn/fc.js
@@ -54,174 +54,77 @@ function setupWeightArrays() {
     var maxWeight, minWeight;
     //console.log('doing setupWeightarrays');
     var i, j;
-    hidden_weights_1a = Create2DArray(nHiddenNodes_1,nPixels);
-    maxWeight = -100;
-    minWeight = 100;
-    for (i=1; i<=nHiddenNodes_1; i++){
-	for (j=1; j<=nPixels; j++){
-	    var weight = hidden_weights_1.e(i,j);
-	    if (weight > maxWeight)
-		maxWeight = weight;
-	    if (weight < minWeight)
-		minWeight = weight;
-	}
-    }
-    for (i=1; i<=nHiddenNodes_1; i++){
-	for (j=1; j<=nPixels; j++){
-	    var weight = hidden_weights_1.e(i,j);
-	    hidden_weights_1a[i-1][j-1] = (weight - minWeight)/(maxWeight-minWeight);
-	}
-    }
-    
-    hidden_weights_2a = Create2DArray(nHiddenNodes_2,nHiddenNodes_1);
-    maxWeight = -100;
-    minWeight = 100;
-    for (i=1; i<=nHiddenNodes_2; i++){
-	for (j=1; j<=nHiddenNodes_1; j++){
-	    var weight = hidden_weights_2.e(i,j);
-	    if (weight > maxWeight)
-		maxWeight = weight;
-	    if (weight < minWeight)
-		minWeight = weight;
-	}
-    }
-    for (i=1; i<=nHiddenNodes_2; i++){
-	for (j=1; j<=nHiddenNodes_1; j++){
-	    var weight = hidden_weights_2.e(i,j);
-	    hidden_weights_2a[i-1][j-1] = (weight - minWeight)/(maxWeight-minWeight);
-	}
-    }
-    
-    final_weightsa = Create2DArray(nFinalNodes,nHiddenNodes_2);
+
+	final_weightsa = Create2DArray(nFinalNodes,nPixels);
     maxWeight = -100;
     minWeight = 100;
     for (i=1; i<=nFinalNodes; i++){
-	for (j=1; j<=nHiddenNodes_2; j++){
-	    var weight = final_weights.e(i,j);
-	    if (weight > maxWeight)
-		maxWeight = weight;
-	    if (weight < minWeight)
-		minWeight = weight;
-	}
+		for (j=1; j<=nPixels; j++){
+			var weight = final_weights.e(i,j);
+			if (weight > maxWeight)
+			maxWeight = weight;
+			if (weight < minWeight)
+			minWeight = weight;
+		}
     }
     for (i=1; i<=nFinalNodes; i++){
-	for (j=1; j<=nHiddenNodes_2; j++){
-	    var weight = final_weights.e(i,j);
-	    final_weightsa[i-1][j-1] = (weight - minWeight)/(maxWeight-minWeight);
-	}
+		for (j=1; j<=nPixels; j++){
+			var weight = final_weights.e(i,j);
+			final_weightsa[i-1][j-1] = (weight - minWeight)/(maxWeight-minWeight);
+		}
     }
     
 }
 
 
 function getNNOutput() {
-    //console.log('getting nn output');
-    var imageData = tinyCtx.getImageData(0, 0, 28, 28);
-    
-    var data = imageData.data;
-    
-    var pixel = 0;
-    var input = new Array(nPixels);
-    
-    for(var i = 0, n = data.length; i < n; i += 4) {
-	if (goodStart){
-	    if (data[i]) {
-		input[pixel] = ((data[i]/255)*1.275)-0.1;// * 1.175;
-	    }
-	    else {
-		input[pixel] = -0.1;
-	    }
-	} else {
-	    input[pixel] = 0;
-	}
-	allNodeInputs[pixel] = data[i]
-	allNodeOutputs[pixel] = input[pixel];
-	allNodeNums[pixel] = pixel+1;
-	pixel++;
+	allZeroes = false;
+    var data = [0, 0, 1, 0];
+
+    for(var i = 0; i < data.length; i++) {
+		allNodeInputs[i] = data[i]
+		allNodeOutputs[i] = data[i];
+		allNodeNums[i] = i+1;
     }
-    for (var i = 0; i < nPixels; i++) {
-	if (input[i] == 0 || input[i] == -0.1) {
-	    allZeroes = true;
-	} else { 
-	    //console.log('found ' + input[i]);
-	    allZeroes = false;
-	    break;
-	}
-    }
-    
-    //console.log(input.length);
-    var input32 = reshapeArray(input);
-    //console.log(input32.length);
-    var inp = Vector.create(input32);
-    //console.log(inp.inspect());
-    var hidden_outputs_1 = Vector.Zero(nHiddenNodes_1);
-    var hidden_outputs_1a = new Array(nHiddenNodes_1);
-    var hidden_outputs_2 = Vector.Zero(nHiddenNodes_2);
-    var hidden_outputs_2a = new Array(nHiddenNodes_2);
+
+    var inp = Vector.create(data);
     var final_outputsa = new Array(nFinalNodes);
     
-    //console.log('there are ' + nHiddenNodes_1 + ' hidden nodes');
-    for (var i=1; i<=nHiddenNodes_1; i++){
-	if (!allZeroes){
-	    var weights = hidden_weights_1.row(i);		  
-	    var sum = inp.dot(weights);
-	    
-	    //console.log('sum for node ' + i + ' = ' + sum);
-	    sum += hidden_biases_1.e(i);
-	    hidden_outputs_1a[i-1] = sigma(sum);
-	    allNodeInputs[nPixels+i-1] = sum;
-	    allNodeOutputs[nPixels+i-1]=hidden_outputs_1a[i-1];
-	    //console.log('output for node ' + i + ' = ' + hidden_outputs_1a[i-1]);
-	} else {
-	    hidden_outputs_1a[i-1] = 0;
-	    allNodeInputs[nPixels+i-1] = 0;
-	    allNodeOutputs[nPixels+i-1]= 0;
-	}
-	allNodeNums[nPixels+i-1] = i;
+    for (var i=1; i<=nFinalNodes; i++){
+		var weights = final_weights.row(i);
+		// var sum = inp.dot(weights);
+		var sum = 1;
+
+		sum += final_biases.e(i);
+		final_outputsa[i-1] = sigma(sum);
+		allNodeInputs[nPixels+i-1] = sum;
+		allNodeOutputs[nPixels+i-1]=final_outputsa[i-1];
+		allNodeNums[nPixels+i-1] = nPixels+i+1;
     }
-    hidden_outputs_1.setElements(hidden_outputs_1a);
     
-    for (i=1; i<=nHiddenNodes_2; i++){
-	if (!allZeroes){
-	    var weights = hidden_weights_2.row(i);				  
-	    var sum = hidden_outputs_1.dot(weights);
-	    sum += hidden_biases_2.e(i);
-	    hidden_outputs_2a[i-1] = sigma(sum);
-	    allNodeInputs[nPixels+nHiddenNodes_1+i-1]=sum;
-	    allNodeOutputs[nPixels+nHiddenNodes_1+i-1]=hidden_outputs_2a[i-1];
-	    
-	} else {
-	    hidden_outputs_2a[i-1] = 0;
-	    allNodeInputs[nPixels+nHiddenNodes_1+i-1]=0;
-	    allNodeOutputs[nPixels+nHiddenNodes_1+i-1]=0;
-	}
-	allNodeNums[nPixels+nHiddenNodes_1+i-1] = i;
-    }
-    hidden_outputs_2.setElements(hidden_outputs_2a);
-    
-    var sums = final_weights.x(hidden_outputs_2);
-    var newSums = sums.add(final_biases);
-    
-    for (i=1; i<=nFinalNodes; i++){
-	if (!allZeroes){
-	    final_outputsa[i-1] = sigma(newSums.e(i));
-	    allNodeInputs[nPixels+nHiddenNodes_1+nHiddenNodes_2+i-1]=newSums.e(i);
-	    allNodeOutputs[nPixels+nHiddenNodes_1+nHiddenNodes_2+i-1]=final_outputsa[i-1];
-	} else {
-	    final_outputsa[i-1] = 0;
-	    allNodeInputs[nPixels+nHiddenNodes_1+nHiddenNodes_2+i-1]=0;
-	    allNodeOutputs[nPixels+nHiddenNodes_1+nHiddenNodes_2+i-1]=0;
-	}
-	allNodeNums[nPixels+nHiddenNodes_1+nHiddenNodes_2+i-1] = i;
-    }
+    // var sums = final_weights.x(hidden_outputs_2);
+    // var newSums = sums.add(final_biases);
+    //
+    // for (i=1; i<=nFinalNodes; i++){
+	// if (!allZeroes){
+	//     final_outputsa[i-1] = sigma(newSums.e(i));
+	//     allNodeInputs[nPixels+nHiddenNodes_1+nHiddenNodes_2+i-1]=newSums.e(i);
+	//     allNodeOutputs[nPixels+nHiddenNodes_1+nHiddenNodes_2+i-1]=final_outputsa[i-1];
+	// } else {
+	//     final_outputsa[i-1] = 0;
+	//     allNodeInputs[nPixels+nHiddenNodes_1+nHiddenNodes_2+i-1]=0;
+	//     allNodeOutputs[nPixels+nHiddenNodes_1+nHiddenNodes_2+i-1]=0;
+	// }
+	// allNodeNums[nPixels+nHiddenNodes_1+nHiddenNodes_2+i-1] = i;
+    // }
     
     allNodeOutputsRaw = allNodeOutputs.slice();
-    normalizeWithinLayer(allNodeOutputs);
+    // normalizeWithinLayer(allNodeOutputs);
     
     if (!allZeroes){
 	var ind1 = maxInd(final_outputsa);
-	finalOutputID = nPixels+nHiddenNodes_1+nHiddenNodes_2+i-1 + ind1 - 10;
-	final_outputsa[ind] = -10;
+	finalOutputID = nPixels+i-1 + ind1 - nFinalNodes;
+	final_outputsa[ind] = -nFinalNodes;
 	var ind2 = maxInd(final_outputsa);
 	document.getElementById("ans1").innerHTML = ind1;
 	document.getElementById("ans2").innerHTML = ind2;
@@ -231,17 +134,9 @@ function getNNOutput() {
     }
     
     isComputed = true;
-    
-    
-    //console.log('officially, output for node 1134 = ' + allNodeOutputsRaw[1134] + ", normalized to " + allNodeOutputs[1134]);
-    
+
     updateCubes();
-    //updateEdges();
-    
-    //console.log(imageData);
-    imageData.data = null;
-    imageData = null;
-    //console.log(imageData);
+    // updateEdges();
 };
 
 function sigma(x) {
